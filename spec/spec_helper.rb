38ee5188567e4f4305f603edd093bf2f8e21b4fc
naoyasakua <4869ns@gmail.com>
2020-11-30 19:18:16 +0900

    Model Specを設定

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
-
 RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by(:selenium_chrome)
+  end
+  
   config.before(:each) do |example|
     driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: { args: ['no-sandbox'] } if example.metadata[:type] == :system
   end
