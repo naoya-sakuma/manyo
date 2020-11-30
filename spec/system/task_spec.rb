f230cd192e8d757f22fc3b8edf1cfc010758aca6
naoyasakua <4869ns@gmail.com>
2020-11-30 18:39:35 +0900

    System Specを記述

---
+++ spec/system/task_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :system do
+  #background do
+    #FactoryBot.create(:task, title: '付け加えた名前１')
+    #FactoryBot.create(:task, title: '付け加えた名前２')
+    #FactoryBot.create(:second_task, title: '付け加えた名前３', content: '付け加えたコンテント')
+  #end
+  before do
+    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+  end
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        # 1. new_task_pathに遷移する（新規作成ページに遷移する）
+        # ここにnew_task_pathにvisitする処理を書く
+        visit new_task_path
+        # 2. 新規登録内容を入力する
+        #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
+        # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+        fill_in 'タスク名', with: 'タスク1'
+        # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+        fill_in 'タスク詳細', with: 'タスク1の詳細'
+        # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
+        # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
+        click_on '登録する'
+        # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
+        # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
+        # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+        expect(page).to have_content 'タスク1'
+        expect(page).to have_content 'タスク1の詳細'
+      end
+    end
+  end
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '作成済みのタスク一覧が表示される' do
+        # テストで使用するためのタスクを作成
+        task = FactoryBot.create(:task, title: 'task')
+        # タスク一覧ページに遷移
+        visit tasks_path
+        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
+        # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
+        expect(page).to have_content 'task'
+        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
+      end
+    end
+  end
+  describe '詳細表示機能' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示される' do
+         # テストで使用するためのタスクを作成
+         task = FactoryBot.create(:task, id: 1, title: 'task')
+         # タスク詳細ページに遷移
+         visit task_path(1)
+         # 遷移したページでtaskがhave_contentされているかexpect(確認)する
+         expect(page).to have_content 'task'
+       end
+     end
+  end
+end
