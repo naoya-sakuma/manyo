712838856dcd4fb93fb812931cf10b7aa67e92d1
naoyasakua <4869ns@gmail.com>
2020-12-01 15:19:28 +0900

    フラッシュメッセージを修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(update_task_params)
-        format.html { redirect_to @task, notice: 'Room was successfully updated.' }
+        format.html { redirect_to @task, notice: 'task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
