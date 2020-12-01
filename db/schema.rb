45b2625e8162e5fed64daace9d5de38d4694294b
naoyasakua <4869ns@gmail.com>
2020-12-01 15:05:50 +0900

    データベースにバリデーションを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_062351) do
+ActiveRecord::Schema.define(version: 2020_12_01_060115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
-    t.text "content"
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
