4116ebc68f6fe8ce6c108d0f64d2263cde3468d5
naoyasakua <4869ns@gmail.com>
2020-12-03 11:29:06 +0900

    マイグレーションファイルを修正

--- db/migrate/20201127062351_change_column_null_tasks.rb
+++ db/migrate/20201127062351_change_column_null_tasks.rb
@@ -1,3 +1,4 @@
 class ChangeColumnNullTasks < ActiveRecord::Migration[5.2]
   change_column :tasks, :title, :string, null: false
+  change_column :tasks, :content, :text, null: false
 end
--- db/migrate/20201201060115_change_column_null_tasks.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNullTasks < ActiveRecord::Migration[5.2]
-  def change
-    change_column :tasks, :content, :text, null: false
-  end
-end
