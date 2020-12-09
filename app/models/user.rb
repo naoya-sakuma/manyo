ccb896e1dde8e7287f9b98df0a4e98810902fd11
naoyasakua <4869ns@gmail.com>
2020-12-09 19:30:17 +0900

    管理者が0人にならないようにコールバックを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,16 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :email, uniqueness: true
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
   has_many :tasks, dependent: :destroy
+
+  before_destroy :admin_check
+
+  def admin_check
+    @user = User.all
+    throw(:abort) if @user.count('true') <= 1
+  end
 end
