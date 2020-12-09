afcf7bf50453f1c980a7d146387923549d7b88e2
naoyasakua <4869ns@gmail.com>
2020-12-09 09:03:47 +0900

    タスク一覧で自分が作成したタスクのみ表示するように設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,17 +4,17 @@ class TasksController < ApplicationController
 
   def index
     if logged_in?
-      @tasks = Task.page(params[:page]).per(PER)
+      @tasks = current_user.tasks.page(params[:page]).per(PER)
         if params[:sort_expired] != nil
-          @tasks = Task.order(expired_at: :asc).page(params[:page]).per(PER)
+          @tasks = current_user.tasks.order(expired_at: :asc).page(params[:page]).per(PER)
         elsif params[:sort_priority] != nil
-          @tasks = Task.order(priority: :asc).page(params[:page]).per(PER)
+          @tasks = current_user.tasks.order(priority: :asc).page(params[:page]).per(PER)
         elsif params[:title].present? && params[:status].present?
-          @tasks = Task.both_title_status(params[:title], params[:status]).page(params[:page]).per(PER)
+          @tasks = current_user.tasks.both_title_status(params[:title], params[:status]).page(params[:page]).per(PER)
         elsif params[:title].present? && params[:status].blank?
-          @tasks = Task.only_title(params[:title]).page(params[:page]).per(PER)
+          @tasks = current_user.tasks.only_title(params[:title]).page(params[:page]).per(PER)
         elsif params[:title].blank? && params[:status].present?
-          @tasks = Task.only_status(params[:status]).page(params[:page]).per(PER)
+          @tasks = current_user.tasks.only_status(params[:status]).page(params[:page]).per(PER)
         end
     else
       redirect_to new_session_path
@@ -30,7 +30,9 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
+    #@task = Task.new(task_params)
+    #@task.user_id = current_user.id
 
     respond_to do |format|
       if @task.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :tasks
 end
