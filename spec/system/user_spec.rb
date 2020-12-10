2bfc264946dd61020e84a929658611b16fa05f87
naoyasakua <4869ns@gmail.com>
2020-12-10 17:31:18 +0900

    管理画面のテストを作成

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -23,7 +23,8 @@ RSpec.describe 'ユーザー登録機能', type: :system do
 
   describe 'セッション機能' do
     before do
-      user = FactoryBot.create(:user)
+      @user = FactoryBot.create(:user)
+      @second_user = FactoryBot.create(:second_user)
       visit new_session_path
       fill_in 'メールアドレス', with: 'test@test.com'
       fill_in 'パスワード', with: 'test@test.com'
@@ -42,8 +43,7 @@ RSpec.describe 'ユーザー登録機能', type: :system do
     end
     context 'ログイン中に他人のマイページに飛んだ場合' do
       it 'タスク一覧に遷移する' do
-        second_user = FactoryBot.create(:second_user)
-        visit user_path(second_user.id)
+        visit user_path(@second_user.id)
         expect(page).to have_content 'タスク一覧'
       end
     end
@@ -55,4 +55,62 @@ RSpec.describe 'ユーザー登録機能', type: :system do
     end
   end
 
+  describe '管理画面' do
+    before do
+      @user = FactoryBot.create(:user)
+      @second_user = FactoryBot.create(:second_user)
+      visit new_session_path
+      fill_in 'メールアドレス', with: 'test@test.com'
+      fill_in 'パスワード', with: 'test@test.com'
+      find('#log_in').click
+    end
+    context '管理ユーザーがログインしている場合' do
+      it '管理画面にアクセスできる' do
+        click_on 'ユーザー管理画面'
+        expect(page).to have_content 'ユーザー管理画面'
+      end
+    end
+    context '一般ユーザーがログインしている場合' do
+      it '管理画面にアクセスできない' do
+        click_on 'ログアウト'
+        fill_in 'メールアドレス', with: 'test2@test.com'
+        fill_in 'パスワード', with: 'test2@test.com'
+        find('#log_in').click
+        visit admin_users_path
+        expect(page).not_to have_content 'ユーザー管理画面'
+      end
+    end
+    context '管理ユーザーがログインしている場合' do
+      it '他ユーザーの詳細画面にアクセスできる' do
+        visit admin_user_path(@second_user.id)
+      end
+    end
+    context '管理ユーザーがログインしている場合' do
+      it '一般ユーザーを編集できる' do
+        visit edit_admin_user_path(@second_user.id)
+        fill_in '名前', with: '編集後テストユーザー2'
+        fill_in 'メールアドレス', with: 'edited_test2@test.com'
+        fill_in 'パスワード', with: 'edited_test2@test.com'
+        fill_in '確認用パスワード', with: 'edited_test2@test.com'
+        fill_in '管理者権限', with: 'false'
+        click_on '新規ユーザー作成'
+        expect(page).not_to have_content '\n編集後テストユーザー2のページ'
+      end
+    end
+    context '管理ユーザーがログインしている場合' do
+      it 'ユーザーを削除できる' do
+        visit edit_admin_user_path(@user.id)
+        fill_in '名前', with: '管理者ユーザー'
+        fill_in 'メールアドレス', with: 'admin_test@test.com'
+        fill_in 'パスワード', with: 'admin_test@test.com'
+        fill_in '確認用パスワード', with: 'admin_test@test.com'
+        fill_in '管理者権限', with: 'true'
+        click_on '新規ユーザー作成'
+        click_on 'ユーザー管理画面'
+        click_on '削除', match: :first
+        page.driver.browser.switch_to.alert.accept
+        expect(page).not_to have_content 'テストユーザー2'
+      end
+    end
+  end
 end
