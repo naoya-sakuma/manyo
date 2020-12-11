01eac4346c52a7bf143896b791d93e18f7dbf1a4
naoyasakua <4869ns@gmail.com>
2020-12-12 07:18:25 +0900

    各モデルに関連付けを指定

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   validates :content, presence: true
 
   belongs_to :user
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
 
   scope :both_title_status, -> title, status { where('title like?', "%#{title}%").where('status like?', "#{status}") }
   scope :only_title, -> title { where('title like?', "%#{title}%")}
